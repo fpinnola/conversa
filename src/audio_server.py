3e8cdb3b9245a1449fb8a76cf92a71cde1e2de8e
Frank Pinnola <frankie4pinnola@gmail.com>
2024-02-24 15:32:40 -0500

    Fix audio websocket endpoint

--- src/audio_server.py
+++ src/audio_server.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI, WebSocket
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import asyncio
-from src.whisper_transcribe import transcribe_audio
+from whisper_transcribe import transcribe_audio
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(process_audio_queue())
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 # Global queue for audio data
 audio_queue = asyncio.Queue()
@@ -13,23 +21,22 @@ async def process_audio_queue():
         audio_data = await audio_queue.get()
         await transcribe_audio(audio_data)
 
-@app.on_event("startup")
-async def startup_event():
-    # Start the audio processing task
-    asyncio.create_task(process_audio_queue())
 
-@app.websocket("/ws/audio")
+@app.websocket("/audio-websocket/ws/audio/{callId}")
 async def websocket_audio_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
             data = await websocket.receive_bytes()
-            await audio_queue.put(data)
-            await websocket.send_text("Data received")
+            print(data)
+            # await audio_queue.put(data)
+            # await websocket.send_text("Data received")
+    except WebSocketDisconnect:
+        print(f"Websocket closed by client")
     except Exception as e:
         print(f"Error: {e}")
-    finally:
         await websocket.close()
+        
 
 
 if __name__ == "__main__":
